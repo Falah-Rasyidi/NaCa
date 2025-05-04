a8c9b596dd3a418a649be24315c5a5dffcdebaac
Falah Rasyidi <s4007133@student.rmit.edu.au>
2025-05-04 15:53:34 +1000

    Using regular queue rather than blocking queue

--- app/ListenerThread.java
+++ app/ListenerThread.java
@@ -3,38 +3,35 @@ package app;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.BlockingQueue;
+import java.util.LinkedList;
 
 /*
  * All this class does is listens for new clients.
- * After a client connects, pass it to the client handler via a blocking queue?
+ * After a client connects, pass it to the client handler via a queue?
  */
 public class ListenerThread implements Runnable {
-    private final BlockingQueue<Socket> CLIENT_QUEUE;
+    private final LinkedList<Socket> CLIENT_QUEUE;
     private final int PORT_NUMBER;
 
-    public ListenerThread(BlockingQueue<Socket> queue, int port) {
+    public ListenerThread(LinkedList<Socket> queue, int port) {
         CLIENT_QUEUE = queue;
         PORT_NUMBER = port;
     }
 
-    
     public void run() {
         try {
             ServerSocket server = new ServerSocket(PORT_NUMBER);
+            System.out.println("Server is listening on port " + PORT_NUMBER);
 
             while (true) {
                 Socket socket = server.accept();
 
-                // After client connects, place into queue and send over to client handler
-                CLIENT_QUEUE.put(socket);
+                // After client connects, create new thread for them and put into queue
+                CLIENT_QUEUE.add(socket);
             }
 
         } catch (IOException e) {
             System.out.println("Error creating server socket or accepting client");
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            System.out.println("Error putting client socket into blocking queue");
             e.printStackTrace();
         }
     }
